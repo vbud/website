4ca8b7a4ba6508069dd2fb1ded49d0907f80c6f3
Valjean Clark <valjeanbud@gmail.com>
2025-08-09 11:14:42 -0700

    add vercel adapter with web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vbud.dev",
   integrations: [sitemap()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    }
+  }),
 });
